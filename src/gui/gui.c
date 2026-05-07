62f6815feba800b7ffe7a659a6d00adfb33127fe
FrederikVA <74868347+FrederikVA@users.noreply.github.com>
2026-05-07 09:44:59 +0200

    For own build three

--- src/gui/gui.c
+++ src/gui/gui.c
@@ -1,7 +1,21 @@
 // gui.c
-#include <SDL.h>
-#include <SDL_ttf.h>
-#include <SDL_image.h>
+#if defined(__has_include)
+#  if __has_include(<SDL2/SDL.h>)
+#    include <SDL2/SDL.h>
+#    include <SDL2/SDL_ttf.h>
+#    include <SDL2/SDL_image.h>
+#  elif __has_include(<SDL.h>)
+#    include <SDL.h>
+#    include <SDL_ttf.h>
+#    include <SDL_image.h>
+#  else
+#    error "SDL2 headers not found"
+#  endif
+#else
+#  include <SDL.h>
+#  include <SDL_ttf.h>
+#  include <SDL_image.h>
+#endif
 #include "gui.h"
 #include "gui_buttons.h"
 #include "stateHandler.h"
